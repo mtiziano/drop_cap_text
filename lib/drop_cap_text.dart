c4c4ced5abd989ff76e3349ec3c01cd7f9e29703
Tiziano Munegato <tiziano.munegato@gmail.com>
2020-01-14 00:47:42 +0100

    RTL + justification + baseline + dropCapPosition + forceNoDescent

--- lib/drop_cap_text.dart
+++ lib/drop_cap_text.dart
@@ -1,12 +1,24 @@
 library drop_cap_text;
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 enum DropCapMode {
+  /// default
   inside,
   upwards,
-  left,
+  aside,
+
+  /// Does not support dropCapPadding, indentation, dropCapPosition and custom dropCap.
+  /// Try using DropCapMode.upwards in combination with dropCapPadding and forceNoDescent=true
+  baseline
+}
+
+enum DropCapPosition {
+  start,
+  end,
 }
 
 class DropCap extends StatelessWidget {
@@ -36,20 +48,25 @@ class DropCapText extends StatelessWidget {
   final DropCap dropCap;
   final EdgeInsets dropCapPadding;
   final Offset indentation;
+  final bool forceNoDescent;
+  final TextDirection textDirection;
+  final DropCapPosition dropCapPosition;
   int dropCapChars;
 
-  DropCapText(
-    this.data, {
-    Key key,
-    this.mode = DropCapMode.inside,
-    this.style,
-    this.dropCapStyle,
-    this.textAlign,
-    this.dropCap,
-    this.dropCapPadding = EdgeInsets.zero,
-    this.indentation = Offset.zero,
-    this.dropCapChars = 1,
-  }) : super(key: key);
+  DropCapText(this.data,
+      {Key key,
+      this.mode = DropCapMode.inside,
+      this.style,
+      this.dropCapStyle,
+      this.textAlign,
+      this.dropCap,
+      this.dropCapPadding = EdgeInsets.zero,
+      this.indentation = Offset.zero,
+      this.dropCapChars = 1,
+      this.forceNoDescent = false,
+      this.textDirection = TextDirection.ltr,
+      this.dropCapPosition})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -57,25 +74,33 @@ class DropCapText extends StatelessWidget {
       color: Colors.black,
       fontSize: 14,
       height: 1,
+      fontFamily: Theme.of(context)?.textTheme?.body1?.fontFamily,
     ).merge(style);
 
     if (data == '') return Text(data, style: textStyle);
 
-    double capWidth, capHeight;
-    double lineHeight = textStyle.fontSize * textStyle.height;
-    double capFontSize = textStyle.fontSize * 5.5;
-    CrossAxisAlignment sideCrossAxisAlignment = CrossAxisAlignment.start;
-
     TextStyle capStyle = TextStyle(
       color: textStyle.color,
-      fontSize: capFontSize,
+      fontSize: textStyle.fontSize * 5.5,
       fontFamily: textStyle.fontFamily,
       fontWeight: textStyle.fontWeight,
       fontStyle: textStyle.fontStyle,
-      height: 0.8,
+      height: 1,
     ).merge(dropCapStyle);
 
-    //if (mode == DropCapMode.baseline) return _buildBaseline(textStyle, capStyle);
+    TextPainter textPainter = TextPainter(
+      textDirection: textDirection,
+      text: TextSpan(
+        text: data.substring(1),
+        style: textStyle.apply(fontSizeFactor: MediaQuery.of(context).textScaleFactor),
+      ),
+    );
+
+    double capWidth, capHeight;
+    double lineHeight = textPainter.preferredLineHeight;
+    CrossAxisAlignment sideCrossAxisAlignment = CrossAxisAlignment.start;
+
+    if (mode == DropCapMode.baseline && dropCap == null) return _buildBaseline(context, textStyle, capStyle);
 
     // custom DropCap
     if (dropCap != null) {
@@ -85,32 +110,29 @@ class DropCapText extends StatelessWidget {
     } else {
       TextPainter capPainter = TextPainter(
         text: TextSpan(text: data.substring(0, dropCapChars), style: capStyle),
-        textDirection: TextDirection.ltr,
+        textDirection: textDirection,
       );
       capPainter.layout();
       capWidth = capPainter.width;
-      capHeight = (capPainter.height * 0.8);
+      capHeight = capPainter.height;
+      if (forceNoDescent) {
+        List<LineMetrics> ls = capPainter.computeLineMetrics();
+        capHeight -= ls.isNotEmpty ? ls[0].descent * 0.95 : capPainter.height * 0.2;
+      }
     }
 
     // compute drop cap padding
     capWidth += dropCapPadding.left + dropCapPadding.right;
     capHeight += dropCapPadding.top + dropCapPadding.bottom;
 
+    // int rows = ((capHeight - indentation.dy) / lineHeight).floor();
     int rows = ((capHeight - indentation.dy) / lineHeight).ceil();
-    if (rows != 0) {
-      rows--;
-    }
 
     // DROP CAP MODE - UPWARDS
     if (mode == DropCapMode.upwards) {
       rows = 1;
       sideCrossAxisAlignment = CrossAxisAlignment.end;
     }
-
-    TextPainter textPainter = TextPainter(
-      text: TextSpan(text: data.substring(1), style: textStyle),
-      textDirection: TextDirection.ltr,
-    );
 
     // BUILDER
     return LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
@@ -122,7 +144,8 @@ class DropCapText extends StatelessWidget {
       int startMillis = new DateTime.now().millisecondsSinceEpoch;
       if (rows > 0) {
         textPainter.layout(maxWidth: boundsWidth);
-        double yPos = (rows + 1) * lineHeight;
+        // double yPos = (rows + 1) * lineHeight;
+        double yPos = rows * lineHeight;
         int charIndex = textPainter.getPositionForOffset(Offset(0, yPos)).offset;
         textPainter.maxLines = rows;
         textPainter.layout(maxWidth: boundsWidth);
@@ -133,34 +156,43 @@ class DropCapText extends StatelessWidget {
       int totMillis = new DateTime.now().millisecondsSinceEpoch - startMillis;
 
       // DROP CAP MODE - LEFT
-      if (mode == DropCapMode.left) charIndexEnd = data.length;
+      if (mode == DropCapMode.aside) charIndexEnd = data.length;
 
       return Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           //Text(totMillis.toString() + ' ms'),
           Row(
+            // textDirection: dropCapPosition != null ? (dropCapPosition == DropCapPosition.start?) : textDirection,
+            textDirection: dropCapPosition == null || dropCapPosition == DropCapPosition.start
+                ? textDirection
+                : (textDirection == TextDirection.ltr ? TextDirection.rtl : TextDirection.ltr),
             crossAxisAlignment: sideCrossAxisAlignment,
             children: <Widget>[
               dropCap != null
                   ? Padding(padding: dropCapPadding, child: dropCap)
                   : Container(
-                      //color: Color(0x44ff8800),
                       width: capWidth,
                       height: capHeight,
                       padding: dropCapPadding,
                       child: RichText(
-                        text: TextSpan(text: data.substring(0, dropCapChars), style: capStyle),
+                        textDirection: textDirection,
+                        text: TextSpan(
+                          text: data.substring(0, dropCapChars),
+                          style: capStyle,
+                        ),
                       ),
                     ),
               Flexible(
-                child: Padding(
+                child: Container(
                   padding: EdgeInsets.only(top: indentation.dy),
+                  height: (lineHeight * rows) + indentation.dy,
                   child: Text(
-                    data.substring(dropCapChars, charIndexEnd),
-                    //rows.toString() + data.substring(2, charIndexEnd),
+                    // data.substring(dropCapChars, charIndexEnd),
+                    data.substring(dropCapChars, data.length), //min(charIndexEnd + 10, data.length)
                     style: textStyle,
                     textAlign: textAlign,
+                    textDirection: textDirection,
                   ),
                 ),
               ),
@@ -172,6 +204,7 @@ class DropCapText extends StatelessWidget {
               data.substring(charIndexEnd).trim(),
               style: textStyle,
               textAlign: textAlign,
+              textDirection: textDirection,
             ),
           ),
         ],
@@ -179,16 +212,21 @@ class DropCapText extends StatelessWidget {
     });
   }
 
-  _buildBaseline(TextStyle textStyle, TextStyle capStyle) {
+  _buildBaseline(BuildContext context, TextStyle textStyle, TextStyle capStyle) {
     return RichText(
+      textAlign: textAlign,
       text: TextSpan(
-        style: TextStyle(color: Colors.black),
+        // style: TextStyle(color: Colors.black),
+        style: textStyle,
         children: <TextSpan>[
           TextSpan(
             text: data.substring(0, dropCapChars),
             style: capStyle.merge(TextStyle(height: 0)),
           ),
-          TextSpan(text: data.substring(dropCapChars), style: textStyle),
+          TextSpan(
+            text: data.substring(dropCapChars),
+            style: textStyle.apply(fontSizeFactor: MediaQuery.of(context).textScaleFactor),
+          ),
         ],
       ),
     );
